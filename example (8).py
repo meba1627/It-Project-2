a6975477d5e16eff34edef803bfd4805275cf282
meba1627 <meba1627@gmail.com>
2025-11-07 10:22:12 +0300

    Add files via upload

--- example (8).py
+++ example (8).py
@@ -1,15 +1,20 @@
 x=int(input("Enter your salary:"))
 if 0<=x<=2000:
-    print("your tax is 0 birr")
+	print("your tax is 0 birr")
+	print("your net salary is",x,"birr")
 elif 2000<x<=4000:
-    print("your tax is",x*0.15,"birr")
+	print("your tax is",x-(x*0.15),"birr")
+	print("your net salary is",x-(x*0.15),"birr")
 elif 4000<x<=7000:
-    print("your tax is",x*0.20,"birr")
+	print("your tax is",x*0.20,"birr")
+	print("your net salary is",x-(x*0.20),"birr")
 elif 7000<x<=10000:
-    print("your tax is",x*0.25,"birr")
+	print("your tax is",x*0.25,"birr")
+	print("your net salary is",x-(x*0.25),"birr")
 elif 10000<x<=14000:
-    print("your tax is",x*0.30,"birr")
-else:print("your tax is",x*0.35,"birr")
+	print("your tax is",x*0.20,"birr")
+	print("your net salary is",x-(x*0.3),"birr")
+else:print("your net salary is",x-(x*0.35),"birr")
     
     
     
